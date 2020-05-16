VI4IO/io500-app

commit 725acb3637614b67ca4ae59467691b464c75602e

main: add IO500_MINWRITE constant
Add a constant for the 300s minimum runtime, rather than
hard-coding it in several places in the code.  It will
eventually also needed for Student Cluster Competition.

Use it to print "-invalid" markers for any subtests that
didn't run for a long enough time.  That should never
happen, but it makes the bad result(s) easier to see.

Signed-off-by: Andreas Dilger <adilger@dilger.ca>

src/phase_dbg.c
@@ -1,13 +1,15 @@
 #include <io500-phase.h>
 
+#define STRINGIFY(foo)  #foo
+
 static ini_option_t option[] = {
-  {"stonewall-time", "The stonewall timer, set to a smaller value for testing", 0, INI_UINT, "300", & opt.stonewall},
+  {"stonewall-time", "The stonewall timer, set to a smaller value for testing", 0, INI_UINT, STRINGIFY(IO500_MINWRITE), & opt.stonewall},
   {NULL} };
 
 
 static void validate(void){
-  if(opt.stonewall != 300 && opt.rank == 0){
-    INVALID("stonewall-time != 300\n");
+  if(opt.stonewall != IO500_MINWRITE && opt.rank == 0){
+    INVALID("stonewall-time != %us\n", IO500_MINWRITE);
   }
 }
 

include/io500-phase.h
@@ -32,6 +32,7 @@ typedef struct{
   io500_phase_score_group group;
 } u_phase_t;
 
+#define IO500_MINWRITE 300
 #define IO500_PHASES (2 + 1 + 2*3 + 1 + 4 + 5)
 
 extern u_phase_t p_opt;

src/main.c
@@ -269,7 +269,10 @@ int main(int argc, char ** argv){
 
         char score_str[40];
         sprintf(score_str, "%f", score);
-        dupprintf("[RESULT] %20s %15s %s : time %.3f seconds\n", phase->name, score_str, phase->name[0] == 'i' ? "GiB/s " : "kIOPS", runtime);
+        dupprintf("[RESULT%s] %20s %15s %s : time %.3f seconds\n",
+		  phase->type == IO500_PHASE_WRITE && runtime < IO500_MINWRITE ?
+			"-invalid" : "",
+		  phase->name, score_str, phase->name[0] == 'i' ? "GiB/s " : "kIOPS", runtime);
       }
       u_hash_update_key_val_dbl(& score_hash, phase->name, score);
     }
@@ -330,7 +333,7 @@ int main(int argc, char ** argv){
     PRINT_PAIR("hash", "%X\n", (int) score_hash);
 
     dupprintf("[SCORE%s] Bandwidth %f GB/s : IOPS %f kiops : TOTAL %f\n",
-      opt.is_valid_run ? "" : " INVALID",
+      opt.is_valid_run ? "" : "-invalid",
       scores[IO500_SCORE_BW], scores[IO500_SCORE_MD], overall_score);
   }
 

IO500/io500

commit bc05b73a206f24d74fa8939c114fe0d043bacf85

For isc20 (#8)
* io500.sh: simplify configuration for users

Move the setup_paths() function to the top of the io500.sh script,
since this should be the only thing that users need to change here.

Add a config-full.ini example file that contains all of the
parameters from "io500 --list" so users don't need to know that.

Fix stringification of IO500_MINWRITE in "io500 --list" output.

Fixes: 725acb363761 ("main: add IO500_MINWRITE constant")
Signed-off-by: Andreas Dilger <adilger@dilger.ca>

* io500.sh: update for isc20 submissions

Update revisions to be used for ISC20 submissions to "io500-isc20".
These do not exist yet, but will be created with the required tag
included, so that users can check out the right version.

Include io500.sh and config.ini files in the result directory/tarball.

Move the remaining configuration of the io500.sh script to the top
of the file so that it is easier for users to edit.

Signed-off-by: Andreas Dilger <adilger@dilger.ca>

config-full.ini
@@ -3,10 +3,8 @@
 datadir = ./datafiles
 # The data directory is suffixed by a timestamp. Useful for running several IO500 tests concurrently.
 timestamp-datadir = TRUE
-
 # The result directory.
 resultdir = ./results
-
 # The result directory is suffixed by a timestamp. Useful for running several IO500 tests concurrently.
 timestamp-resultdir = TRUE
 # The API to create/delete the datadir
@@ -19,8 +17,8 @@ drop-caches-cmd = sudo -n bash -c "echo 3 > /proc/sys/vm/drop_caches"
 verbosity = 1
 
 [debug]
-# The stonewall timer, must be at least 300s for a valid test run
-stonewall-time = 300 # 300s for full run
+# Stonewall timer must be 300 for a valid result, can be smaller for testing
+stonewall-time = 300
 
 [ior-easy]
 # The API to be used
@@ -30,13 +28,25 @@ posix.odirect =
 # Transfer size
 transferSize = 2m
 # Block size; must be a multiple of transferSize
-blockSize = 2m # 1024000m
+blockSize = 9920000m
 # Filename for MPI hint file
 hintsFileName = 
 # Create one file per process
 filePerProc = TRUE
 # Use unique directory per file per process
 uniqueDir = FALSE
+# Disable running of this phase
+noRun = 
+# The verbosity level
+verbosity = 
+
+[ior-easy-write]
+# The API to be used
+API = 
+# Use ODirect
+posix.odirect = 
+# Filename for hints file
+hintsFileName = 
 
 [mdtest-easy]
 # The API to be used
@@ -45,14 +55,64 @@ API = POSIX
 posix.odirect = 
 # Files per proc
 n = 1000000
+# Disable running of this phase
+noRun = 
+
+[mdtest-easy-write]
+# The API to be used
+API = 
+# Use ODirect
+posix.odirect = 
+# Disable running of this phase
+noRun =
 
 [ior-hard]
+# The API to be used
+API = POSIX
+# Use ODirect
+posix.odirect = 
 # Filename for hints file
 hintsFileName = 
 # Number of segments
 segmentCount = 10000000
+# Disable running of this phase
+noRun =
+# The verbosity level
+verbosity = 
+
+[ior-hard-write]
+# The API to be used
+API = 
+# Use ODirect
+posix.odirect = 
+# Filename for hints file
+hintsFileName = 
+
+[mdtest-hard]
+# The API to be used
+API = POSIX
+# Use ODirect
+posix.odirect = 
+# Files per proc
+n = 1000000
+# Disable running of this phase
+noRun =
+
+[mdtest-hard-write]
+# The API to be used
+API = 
+# Use ODirect
+posix.odirect = 
+# Disable running of this phase
+noRun =
 
 [find]
+# Set to an external script to perform the find phase
+external-script = 
+# Extra arguments for external scripts
+external-extra-args = 
+# Startup arguments for external scripts
+external-mpi-args = 
 # Set the number of processes for pfind
 nproc = 
 # Pfind queue length
@@ -60,4 +120,4 @@ pfind-queue-length = 10000
 # Pfind Steal from next
 pfind-steal-next = FALSE
 # Parallelize the readdir by using hashing. Your system must support this!
-pfind-parallelize-single-dir-access-using-hashing = TRUE
+pfind-parallelize-single-dir-access-using-hashing = FALSE

io500.sh
@@ -3,8 +3,6 @@
 # INSTRUCTIONS:
 # This script takes its parameters from the same .ini file as io500 binary.
 
-set -eo pipefail  # better error handling
-
 function setup_paths {
   # Set the paths to the binaries and how to launch MPI jobs.
   # If you ran ./utilities/prepare.sh successfully, then binaries are in ./bin/
@@ -14,6 +12,34 @@ function setup_paths {
   io500_mpirun="mpiexec"
   io500_mpiargs="-np 2"
 }
+
+function setup_directories {
+  local workdir
+  local resultdir
+  local ts
+
+  # set directories where benchmark files are created and where the results go
+  # If you want to set up stripe tuning on your output directories or anything
+  # similar, then this is the right place to do it.  This creates the output
+  # directories for both the app run and the script run.
+
+  timestamp=$(date +%Y.%m.%d-%H.%M.%S)           # create a uniquifier
+  [ $(get_ini_global_param timestamp-datadir True) != "False" ] &&
+	ts="$timestamp" || ts="io500"
+  # directory where the data will be stored
+  workdir=$(get_ini_global_param datadir $PWD/datafiles)/$ts
+  io500_workdir=$workdir-scr
+  [ $(get_ini_global_param timestamp-resultdir True) != "False" ] &&
+	ts="$timestamp" || ts="io500"
+  # the directory where the output results will be kept
+  resultdir=$(get_ini_global_param resultdir $PWD/results)/$ts
+  io500_result_dir=$resultdir-scr
+
+  mkdir -p $workdir-{scr,app} $resultdir-{scr,app}
+}
+
+# you should not edit anything below this line
+set -eo pipefail  # better error handling
 
 io500_ini="${1:-""}"
 if [[ -z "$io500_ini" ]]; then
@@ -126,24 +152,6 @@ function main {
   create_tarball
 }
 
-function setup_directories {
-  local dir
-  local ts
-
-  # set directories for where the benchmark files are created and where the results will go.
-  # If you want to set up stripe tuning on your output directories or anything similar, then this is good place to do it.
-  timestamp=$(date +%Y.%m.%d-%H.%M.%S)           # create a uniquifier
-  [ $(get_ini_global_param timestamp-datadir True) != "False" ] &&
-	ts="$timestamp" || ts="io500"
-  # directory where the data will be stored
-  io500_workdir=$(get_ini_global_param datadir $PWD/datafiles)/$ts-scr
-  [ $(get_ini_global_param timestamp-resultdir True) != "False" ] &&
-	ts="$timestamp" || ts="io500"
-  # the directory where the output results will be kept
-  io500_result_dir=$(get_ini_global_param resultdir $PWD/results)/$ts-scr
-  mkdir -p $io500_workdir $io500_result_dir
-}
-
 function setup_ior_easy {
   local params
 
@@ -233,6 +241,7 @@ create_tarball() {
   local fname=$(basename ${io500_result_dir%-scr})
   local tarball=$sourcedir/io500-$HOSTNAME-$fname.tgz
 
+  cp -v $0 $io500_ini $io500_result_dir
   tar czf $tarball -C $sourcedir $fname-{app,scr}
   echo "Created result tarball $tarball"
 }

prepare.sh
@@ -7,9 +7,9 @@ echo It will also attempt to build the benchmarks
 echo It will output OK at the end if builds succeed
 echo
 
-IOR_HASH=
-IO500_HASH=
-MDREAL_HASH=
+IOR_HASH=io500-isc20
+IO500_HASH=io500-isc20
+MDREAL_HASH=io500-isc20
 
 INSTALL_DIR=$PWD
 BIN=$INSTALL_DIR/bin

src/phase_dbg.c
@@ -1,9 +1,10 @@
 #include <io500-phase.h>
 
-#define STRINGIFY(foo)  #foo
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
 
 static ini_option_t option[] = {
-  {"stonewall-time", "The stonewall timer, set to a smaller value for testing", 0, INI_UINT, STRINGIFY(IO500_MINWRITE), & opt.stonewall},
+  {"stonewall-time", "Stonewall timer must be "TOSTRING(IO500_MINWRITE)" for a valid result, can be smaller for testing", 0, INI_UINT, TOSTRING(IO500_MINWRITE), & opt.stonewall},
   {NULL} };
 
 
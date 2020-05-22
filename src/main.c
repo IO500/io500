25a1e1da67b5bfdfc756bf6a6d53fa27bfbf19ee
Andreas Dilger <adilger@dilger.ca>
2020-05-21 22:22:38 -0600

    io500: fix bug in printf format
    Fix small bug introduced in previous patch.
    
    Fixes: 50c7809bdb ("io500: avoid segfault when parsing bad .ini input")
    Signed-off-by: Andreas Dilger <adilger@dilger.ca>

--- src/main.c
+++ src/main.c
@@ -98,7 +98,7 @@ int main(int argc, char ** argv){
   if (argc < 2 || strcmp(argv[1], "-l") == 0 || strcmp(argv[1], "--list") == 0){
     if (rank == 0){
       /* print this as a comment, in case it is saved into the .ini file */
-      r0printf(stderr, "# Supported and current values of the ini file:\n");
+      r0printf("# Supported and current values of the ini file:\n");
       u_ini_print_values(stdout, cfg, TRUE);
     }
     goto out;

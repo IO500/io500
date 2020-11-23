f1ab9148ebd8dbd0b86c8e4d2dc72c5c258b64bb
Julian M. Kunkel <juliankunkel@googlemail.com>
2020-11-23 23:43:59 +0000

    Allow mdworkbench to inherit API used.

--- src/phase_mdworkbench.c
+++ src/phase_mdworkbench.c
@@ -13,7 +13,7 @@ It attempts to run pre-create for 60s.
 opt_mdworkbench mdworkbench_o;
 
 static ini_option_t option[] = {
-  {"API", "The API to be used", 0, INI_STRING, "POSIX", & mdworkbench_o.api},
+  {"API", "The API to be used", 0, INI_STRING, NULL, & mdworkbench_o.api},
   {"waitingTime", "Waiting time of an IO operation relative to runtime (1.0 is 100%%)", 0, INI_FLOAT, "0.0", & mdworkbench_o.waiting_time},
   {"precreatePerSet", "Files to precreate per set (always 10 sets), this is normally dynamically determined", 0, INI_UINT64, NULL, & mdworkbench_o.precreate_per_set},
   {"filesPerProc", "Files to run per iteration and set (always 10 sets), this is normally dynamically determined", 0, INI_UINT64, NULL, & mdworkbench_o.files_per_proc},
@@ -46,17 +46,16 @@ void mdworkbench_process(u_argv_t * argv, FILE * out, mdworkbench_results_t ** r
 
 
 void mdworkbench_add_params(u_argv_t * argv){
-  opt_mdworkbench d = mdworkbench_o;
+  opt_mdworkbench * d = & mdworkbench_o;
 
   u_argv_push(argv, "./md-workbench");
   for(int i=0; i < mdworkbench_o.verbosity; i++){
     u_argv_push(argv, "-v");
   }
   u_argv_push(argv, "--process-reports");
-  u_argv_push(argv, "-a");
-  u_argv_push(argv, d.api);
+  u_argv_push_default_if_set(argv, "-a", opt.api, d->api);
   u_argv_push_printf(argv, "-o=%s/mdworkbench", opt.datadir);
-  u_argv_push_printf(argv, "-t=%f", d.waiting_time);
+  u_argv_push_printf(argv, "-t=%f", d->waiting_time);
   u_argv_push(argv, "-O=1");
   u_argv_push_printf(argv, "--run-info-file=%s/mdworkbench.status", opt.resdir );
 
@@ -65,9 +64,9 @@ void mdworkbench_add_params(u_argv_t * argv){
   uint64_t precreate_per_set;
   uint64_t files_per_proc;
 
-  if (d.precreate_per_set != INI_UNSET_UINT64 && d.files_per_proc != INI_UNSET_UINT64){
-    precreate_per_set = d.precreate_per_set;
-    files_per_proc = d.files_per_proc;
+  if (d->precreate_per_set != INI_UNSET_UINT64 && d->files_per_proc != INI_UNSET_UINT64){
+    precreate_per_set = d->precreate_per_set;
+    files_per_proc = d->files_per_proc;
   }else{
     mdtest_generic_res* mdtest = mdtest_easy_write_get_result();
     if( mdtest->rate <= 0.0 ){

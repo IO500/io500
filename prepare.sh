cf79a6ee6da5caa8308543bb99f457b0e8b89bac
Julian M. Kunkel <juliankunkel@googlemail.com>
2025-05-16 08:36:29 +0200

    Update IOR to newest version to include fix for rand64.

--- prepare.sh
+++ prepare.sh
@@ -7,7 +7,7 @@ echo It will also attempt to build the benchmarks
 echo It will output OK at the end if builds succeed
 echo
 
-IOR_HASH=14bdbfb
+IOR_HASH=348572ccfe8963
 PFIND_HASH=778dca8
 
 INSTALL_DIR=$PWD

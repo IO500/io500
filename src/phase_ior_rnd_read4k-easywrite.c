5c161850846997603f38eb7269eacd094cf00bd2
Julian M. Kunkel <julian.kunkel@gwdg.de>
2026-04-01 08:35:54 +0200

    Support shifting one more node away for random read.

--- src/phase_ior_rnd_read4k-easywrite.c
+++ src/phase_ior_rnd_read4k-easywrite.c
@@ -47,6 +47,7 @@ static double run(void){
 
   if (d.filePerProc){
     u_argv_push(argv, "-C");	/* reorder tasks in constant order for read */
+    u_argv_push(argv, "-C");	/* shift one more node away */
     u_argv_push(argv, "-Q");	/* task per node offset */
     u_argv_push(argv, "1");
   }
